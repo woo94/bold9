29a94943525e1cea23b0fa919b28dc05548e3658
woo94 <bestman21c@naver.com>
2021-12-31 10:35:35 +0900

    seed with factory functions and createMany Prisma Client API

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,61 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+const userFactory = (name: string, email?: string) => {
+    return {
+        email: email ?? `${name}@gmail.com`,
+        name,
+        password: `${name}${name}`
+    }
+}
+
+const postFactory = (title: string, content: string, authorId: number, published: boolean = true) => {
+    return {
+        title,
+        content,
+        authorId,
+        published
+    }
+}
+
+const commentFactory = (postId: number, content: string) => {
+    return {
+        postId,
+        content
+    }
+}
+
 async function main() {
-    const createUsers = await prisma.user.createMany({
+    await prisma.user.createMany({
         data: [
-            { email: 'hazel@gmail.com', password: 'hazelhazel', name: 'hazel' },
-            { email: 'amelia@gmail.com', password: 'ameliaamelia', name: 'amelia' },
-            { email: 'lily@gmail.com', password: 'lilylily', name: 'lily' },
-            { email: 'grace@gmail.com', password: 'gracegrace', name: 'grace' },
-            { email: 'oliver@gmail.com', password: 'oliveroliver', name: 'oliver' },
-            { email: 'liam@gmail.com', password: 'liamliam', name: 'liam' },
-            { email: 'lucas@gmail.com', password: 'lucaslucas', name: 'lucas' },
-            { email: 'bestman21c@gmail.com', password: 'andrewandrew', name: 'andrew' }
+            userFactory('hazel'),
+            userFactory('amelia'),
+            userFactory('lily'),
+            userFactory('grace'),
+            userFactory('oliver'),
+            userFactory('liam'),
+            userFactory('lucas'),
+            userFactory('andrew', 'bestman21c@gmail.com')
         ]
     })
 
-    const createPosts = await prisma.post.createMany({
+    await prisma.post.createMany({
         data: [
-            { title: 'hazel init', content: 'this is goood', authorId: 1 },
-            { title: 'hazel boring', content: 'anyone to play with?', authorId: 2 },
-            { title: 'taking a test', content: 'very very tough!', authorId: 8 }
+            postFactory('hazle say hi', 'hi my name is hazel. i am new here', 1),
+            postFactory('hazle boring', 'anyone to play with?', 1),
+            // create posts with content contains graphql and published false/true
+            postFactory("who's got cheatsheet?", 'help me with graphql!!!!', 8, false),
+            postFactory("taking a test", "graphql is tough. actually all other stuff is tough either", 8, true)
         ]
     })
 
-    console.log(createUsers, createPosts)
+    await prisma.comment.createMany({
+        data: [
+            commentFactory(3, 'email me'),
+            commentFactory(3, 'here is a link. it will help you a lot'),
+            commentFactory(4, 'cheer up andrew T.T')
+        ]
+    })
 }
 
 main().catch(err => {
